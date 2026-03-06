8e36f8ff34c5262798b0ed2d88d0101b9bbe55ee
rupam.mandal.cse25 <rupam.mandal.cse25@itbhu.ac.in>
2026-03-06 10:57:56 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rupam Mandal",
+    author_email="rupam.mandal.cse25@itbhu.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
